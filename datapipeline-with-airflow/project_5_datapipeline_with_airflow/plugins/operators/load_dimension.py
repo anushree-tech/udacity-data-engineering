f48b14de69c6029cfc385f1f7f0ada0857abbcf6
anushree-tech <anushreeacharya1@gmail.com>
2020-04-11 14:54:13 +0530

    Add datapipeline with airflow project

---
+++ datapipeline-with-airflow/project_5_datapipeline_with_airflow/plugins/operators/load_dimension.py
@@ -0,0 +1,41 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from helpers import SqlQueries
+
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+    insert_into_stmt = """
+        INSERT INTO {table} 
+        {select_query}
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table,
+                 sql,
+                 append_only = False,
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.sql = sql
+        self.append_only = append_only
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        if not self.append_only:
+            self.log.info("Delete {} dimension table".format(self.table))
+            redshift.run("DELETE FROM {}".format(self.table))         
+        self.log.info("Insert data from staging tables into {} dimension table".format(self.table))
+        redshift.run(LoadDimensionOperator.insert_into_stmt.format(
+            table=self.table,
+            select_query=getattr(SqlQueries,self.sql)
+        ))
